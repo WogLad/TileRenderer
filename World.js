0dbc4654cd00e09282d8e158a0a3ffe45600e5e6
WogLad <aaronbusiness2005@gmail.com>
2022-01-12 12:01:42 +0400

    Changed the Way LivingBeings are Stored

--- World.js
+++ World.js
@@ -1,40 +1,23 @@
-var livingBeings = {
-    "humans": [],
-    "elves": [],
-    "dwarves": []
-};
-
-function getAllLivingBeings() {
-    // var l = [];
-    // Object.values(livingBeings).forEach(lbType => {
-    //     lbType.forEach(lb => {
-    //         l.push(lb);
-    //     });
-    // });
-    // return l;
-    return Object.values(livingBeings).flat();
-}
+var livingBeings = [];
 
 function getRandomLivingBeing() {
-    var lbs = Object.values(livingBeings).flat();
-    return lbs[Math.round(Math.random() * (lbs.length-1))];
+    return livingBeings[Math.round(Math.random() * (livingBeings.length-1))];
 }
 
 // Spawn the first generation of humans & other species and let them live out their lives as designed
 function populateWorld(startingAmount) {
+    var species = [
+        Human,
+        Elf,
+        Dwarf
+    ]
     for (var i = 0; i < startingAmount; i++) {
-        livingBeings["humans"].push(new Human());
-    }
-    for (var i = 0; i < startingAmount; i++) {
-        livingBeings["elves"].push(new Elf());
-    }
-    for (var i = 0; i < startingAmount; i++) {
-        livingBeings["dwarves"].push(new Dwarf());
+        livingBeings.push(new species[Math.round(Math.random() * (species.length-1))])
     }
 }
 
-setTimeout(populateWorld(4), 1000);
-console.log(getAllLivingBeings());
+setTimeout(populateWorld(12), 1000);
+console.log(livingBeings);
 
 // World Tick Function for World Clock
 var ticks = 0; // Also is the number for the time of each day in the simulation, measured in ticks of course
@@ -45,10 +28,9 @@ var tickLoop = setInterval(() => {
     // Increase the timer for the clock, which is then used by all the other functions to know the time passed
     if (ticks == 24000) {ticks = 0; console.log("A new day has begun."); console.log(new Date()); /*clearInterval(tickLoop);*/}
 
-    var allLivingBeings = getAllLivingBeings();
-    for (var i = 0; i < allLivingBeings.length; i++) {
-        if (allLivingBeings[i].isAlive == true) {
-            allLivingBeings[i].performWork(ticks);
+    for (var i = 0; i < livingBeings.length; i++) {
+        if (livingBeings[i].isAlive == true) {
+            livingBeings[i].performWork(ticks);
         }
     }
 
