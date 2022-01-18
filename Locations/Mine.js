a3c6f9b7f487d8ab14808b82645c60fbe7bdd446
WogLad <aaronbusiness2005@gmail.com>
2022-01-18 23:34:43 +0400

    Set Up the To-Do(s) For the Mountain Location

---
+++ Locations/Mine.js
@@ -0,0 +1,15 @@
+/*
+    (TODO): Make global variables containing information about the mountain.
+
+    (TODO): Make a "MountainSection" class that contains a random amount of rocks within a range and places them on a two-dimensional
+    grid(array), and keep track of all the miners that are in that specific "MountainSection" class instance.
+
+    (TODO): Assign all the rocks in the mountain section to a single miner each.
+
+    (TODO): Make a hard-coded two-dimensional array where each element contains an instance of the "MountainSection" class if there
+    is a mountain section at that coordinate.
+
+    (TODO): Draw the background, rocks, and miners in the mountain section currently selected onto the canvas.
+
+    (TODO): Make the "mountainMap" be dynamically generated.
+*/
