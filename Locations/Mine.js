166249912e756dea50e83744f29846157a0fa478
WogLad <aaronbusiness2005@gmail.com>
2022-01-18 23:46:28 +0400

    Added the "MountainSection" Class & "generateMountain" Function

--- Locations/Mine.js
+++ Locations/Mine.js
@@ -1,12 +1,12 @@
 /*
     (DONE): Make global variables containing information about the mountain.
 
-    (TODO): Make a "MountainSection" class that contains a random amount of rocks within a range and places them on a two-dimensional
-    grid(array), and keep track of all the miners that are in that specific "MountainSection" class instance.
+    (WIP): Make a "MountainSection" class that contains a random amount of rocks within a range and sets their position in the
+    mountain section, and keep track of all the miners that are in that specific "MountainSection" class instance.
 
     (TODO): Assign all the rocks in the mountain section to a single miner each.
 
-    (TODO): Make a hard-coded two-dimensional array where each element contains an instance of the "MountainSection" class if there
+    (DONE): Make a hard-coded two-dimensional array where each element contains an instance of the "MountainSection" class if there
     is a mountain section at that coordinate.
 
     (TODO): Draw the background, rocks, and miners in the mountain section currently selected onto the canvas.
@@ -21,3 +21,26 @@ var mountainMap = [
     [0, 0, 0, 1, 0],
     [0, 0, 1, 1, 0],
 ];
+
+class MountainSection {
+    constructor() {
+        this.rocks = [
+            // Format:- {x: 0, y: 0}
+        ];
+        this.miners = [
+            // Format:- {<instance of miner>: {x: 0, y: 0}}
+        ];
+    }
+}
+
+function generateMountain() {
+    for (var x = 0; x < mountainMap[0].length; x++) {
+        for (var y = 0; y < mountainMap.length; y++) {
+            if (mountainMap[x][y] == 1) {
+                mountainMap[x][y] = new MountainSection();
+            }
+        }
+    }
+}
+
+getGeneratedMountain();
